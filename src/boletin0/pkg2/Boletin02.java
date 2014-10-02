de5e001bfaa021c1e326bc57358d421faa6a6e09
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-02 12:20:29 +0200

    area1

---
+++ src/boletin0/pkg2/Boletin02.java
@@ -0,0 +1,14 @@
+// clacular o area dun rectangulo
+package boletin0.pkg2;
+
+
+public class Boletin02 {
+
+
+    public static void main(String[] args) {
+    float base=4.0f,altura=6f,area;
+    area=base*altura;
+    System.out.println("area="+ area);
+    }
+    
+}
