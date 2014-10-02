4f9a60d4b83c622c8c3fd1a4921ae5b0c61c8df3
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-02 12:27:57 +0200

    area2

--- src/boletin0/pkg2/Boletin02.java
+++ src/boletin0/pkg2/Boletin02.java
@@ -6,8 +6,9 @@ public class Boletin02 {
 
 
     public static void main(String[] args) {
-    float base=4.0f,altura=6f,area;
-    area=base*altura;
+    float base=4.0f,altura=6f;
+    int area;
+    area=(int)(base*altura);
     System.out.println("area="+ area);
     }
     
